3438b1a08435b639943a51932a59695ade5abf3c
Silvan Moura <silvancortesmoura@outlook.com>
2024-01-01 17:52:53 -0300

    Creation of the getInfoProducts method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function getInfoProducts()
+    {
+        $infosProducts = Product::select('*')->get();
+        return view('products', ['infoProducts' => $infosProducts]);
+    }
 }
