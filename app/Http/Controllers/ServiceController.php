b0fb8d28be574af7460e343cb1c9c1ccc5f5f0ef
Silvan Moura <silvancortesmoura@outlook.com>
2024-01-13 11:41:26 -0300

    Creation of the getInfoServices method

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Os;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    
+    public function getInfoServices()
+    {
+        $getOsOrcamentos = Os::where('operacao_os_id', 1);
+
+        $getOsServicos = Os::select('id')
+            ->where(function ($query) {
+                $query->where('data_entrega', null);
+            })
+            ->where('status_os_id', 3);
+
+        return view('services', ['infoOsOrcamentos' => $getOsOrcamentos, 'infoOsServices' => $getOsServicos]);
+    }
 }
