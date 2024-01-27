df6d837490d697edd0558134f9a456519d9d3cc5
Silvan Moura <silvancortesmoura@outlook.com>
2024-01-27 18:59:23 -0300

    Creation of OsController

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ServiceController;
-use App\Models\Product;
+
 use Illuminate\Support\Facades\Route;
 
 /*
