4827681c688b9ab9785e67bde5a735e2038cc563
Silvan Moura <silvancortesmoura@outlook.com>
2024-01-30 13:31:56 -0300

    Creation of the getInfoCity method and its logic

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
+use App\Models\State;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
-    //
+    public function getInfoCity(){
+        $cities = City::select('*')->get();
+        $states = State::select('*')->get();
+        
+        foreach ($cities as $key => $city) {
+            $state = State::select('*')->where('id', $city->estado_id)->first();
+
+            $cities[$key]['estado_id'] = $state->id;
+            $cities[$key]['estado_nome'] = " $state->sigla - $state->nome";
+        }
+
+        //return $cities;
+        return view('city', ["cities" => $cities, "states" => $states]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CityController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ClientController;
