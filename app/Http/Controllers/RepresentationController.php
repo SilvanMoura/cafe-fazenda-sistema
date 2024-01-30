dfa0cf9530d879a7505191caec8b96af4e2f3816
Silvan Moura <silvancortesmoura@outlook.com>
2024-01-29 22:50:37 -0300

    Creation of the getInfoRepresentation method

--- app/Http/Controllers/RepresentationController.php
+++ app/Http/Controllers/RepresentationController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Representation;
 use Illuminate\Http\Request;
 
 class RepresentationController extends Controller
 {
-    //
+    public function getInfoRepresentation(){
+        $representations = Representation::select('*')->get();
+        //return $manufacturers;
+        return view('representation', ["representations" => $representations]);
+    }
 }
