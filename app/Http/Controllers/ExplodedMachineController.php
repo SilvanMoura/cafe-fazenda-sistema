91963aa5feb67a95a720f00737d40f1bbc8ec3f6
Silvan Moura <silvancortesmoura@outlook.com>
2024-01-31 13:08:21 -0300

    Creation of the getInfoExplodedMachine method and its logic

--- app/Http/Controllers/ExplodedMachineController.php
+++ app/Http/Controllers/ExplodedMachineController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Exploded_machine;
+use App\Models\Manufacturer;
 use Illuminate\Http\Request;
 
 class ExplodedMachineController extends Controller
 {
-    //
+    public function getInfoExplodedMachine(){
+        $getExplanation = Exploded_machine::orderByDesc('id')->get();
+        $manufactures = Manufacturer::select('*')->get();
+
+        foreach ($getExplanation as $key => $explanation) {
+            $explanation = Manufacturer::select('nome')->where('id', $explanation->fabricante_id)->first();
+
+            $getExplanation[$key]['fabricante_id'] = $explanation->nome;
+        }
+
+        //return $getExplanation;
+        return view('exploded-machine', ["infEexplanation" => $getExplanation, "manufactures" => $manufactures]);
+    }
 }
