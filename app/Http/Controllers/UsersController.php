4bc1157da76ef7fcab8bd1e77e897fd86d9a1bf6
Silvan Moura <silvancortesmoura@outlook.com>
2024-02-24 19:01:26 -0300

    Creating the createUser method

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,11 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UsersController extends Controller
 {
-    public function getUsers(){
+    public function getUsers()
+    {
         return view('users');
+    }
+
+    public function createUser(Request $request)
+    {
+        User::create([
+            'name' => $request->input('userNameModelo'),
+            'email' => $request->input('userNameModelo')."@cafedafazenda.com",
+            'password' => Hash::make('123456')
+        ]);
+
+        return response()->json(['message' => 'Usuário registrado com sucesso'], 201);
     }
 }
